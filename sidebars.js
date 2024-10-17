e276334a7c802d17012cf657af4e64c3a8890c85
Keys Bui <keysbui@gmail.com>
2024-10-18 01:49:29 +0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -5,24 +5,6 @@ module.exports = {
     {
       type: "doc",
       id: "index",
-    },
-    {
-      type: "category",
-      label: "Data Structures",
-      items: [
-        "data-structures/stack",
-        "data-structures/queue",
-        "data-structures/linkedlist",
-        "data-structures/hash-table",
-      ]
-    },
-    {
-      type: "category",
-      label: "Algorithms",
-      items: [
-        "algorithms/bfs",
-        "algorithms/dfs",
-      ]
     }
   ]
 };
